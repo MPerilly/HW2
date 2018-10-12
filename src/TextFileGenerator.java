0c6c2009d767e3634daf4153f8a0972f0e061f6c
Michael Perillo <msp501@nyu.edu>
2018-10-11 20:23:43 -0400

    Created methods for generating free timeslots as well as stripping and dressing them for output in a similar fashion to the days of the week

--- src/TextFileGenerator.java
+++ src/TextFileGenerator.java
@@ -46,7 +46,7 @@ public class TextFileGenerator {
     }
     //Generating free days for students:
     public int[][] genFreeDays() {
-        Random r = new Random((randSeed +1));
+        Random r = new Random((this.randSeed +1));
         int[][] studentAvailDays = new int[this.numberOfStudents][5];
         for (int i = 0; i < this.numberOfStudents; i++) {
             int numOfDaysFree = r.nextInt(5);
@@ -99,5 +99,51 @@ public class TextFileGenerator {
             }
         }
         return letterDays;
+    }
+    //Generating free times for students:
+    public int[][][] genFreeTimes() {
+        Random r = new Random((this.randSeed + 2));
+        int[][][] freeTimes = new int[this.numberOfStudents][5][15];
+        for (int i = 0; i < this.numberOfStudents; i++) {
+            for (int j = 0; j < 5; j++){
+                int numberFreeTimes = r.nextInt(15);
+                for (int k = 0; k < numberFreeTimes; k++) {
+                    int freeSlot = r.nextInt(15);
+                    freeTimes[i][j][freeSlot] = 1;
+                }
+            }
+        }
+        return freeTimes;
+    }
+    public int[] stripFreeTimes(int[][][] freeTimes, int studentIndex, int dayIndex) {
+        int[] specificFreeTimes = new int[15];
+        for (int i = 0; i < 15; i++) {
+            specificFreeTimes[i] = freeTimes[studentIndex][dayIndex][i];
+        }
+        return specificFreeTimes;
+    }
+    public String[] convertFreeTimes(int[] specificFreeTimes) {
+        String[] convertedTimes;
+        int freeTimes = 0;
+        int freeTimesIndex = 0;
+        for (int i = 0; i < 15; i++) {
+            if (specificFreeTimes[i] == 1) {
+                freeTimes++;
+            }
+        }
+        convertedTimes = new String[freeTimes];
+        for (int i = 0; i < 15; i++) {
+            if (specificFreeTimes[i] == 1) {
+                if (i == 0) {
+                    convertedTimes[freeTimesIndex] = "0" + (i + 9) + "000";
+                    freeTimesIndex++;
+                }
+                else {
+                    convertedTimes[freeTimesIndex] = (i + 9) + "000";
+                    freeTimesIndex++;
+                }
+            }
+        }
+        return convertedTimes;
     }
 }
